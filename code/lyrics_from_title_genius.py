e303252180d8166f7e38f06eb44cdd4ad220d716
Tzumaki <simone.salamone@studio.unibo.it>
2024-03-25 19:02:40 +0100

    Aggiunto testo da titolo Genius API.

--- code/lyrics_from_title_genius.py
+++ code/lyrics_from_title_genius.py
@@ -3,12 +3,9 @@ In order to run this code without error you need to install the next library:
 - pip install lyricsgenius.
 """
 import lyricsgenius
-token = "zU2Vm2ed_USDRB60JfY3fJlGR7yTY5_xZs79iNoCFciHeaJyRviIK7ev_NYDf5U4"
+token = "zU2Vm2ed_USDRB60JfY3fJlGR7yTY5_xZs79iNoCFciHeaJyRviIK7ev_NYDf5U4" #soxinem's token.
 genius = lyricsgenius.Genius(token)
 
-#artist = genius.search_artist("Taylor Swift")
-#song = artist.song("Blank Space")
-# or:
 song = genius.search_song("Blank Space", "Taylor Swift")
 print(song.lyrics)
 

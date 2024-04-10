0d3717b9708581103e4d695465ac078dd0cf82a1
Alessandro <alex.tomaiuolo22@gmail.com>
2024-04-10 10:25:50 +0200

    Cambiata libreria lyrics

--- code/lyrics_from_title_lyrist.py
+++ code/lyrics_from_title_lyrist.py
@@ -5,14 +5,12 @@ Please notice that this API (lyrist) permits only 150 per hour.
 import requests
 from string import Template #using for string interpolation
 
-track = "Sere Nere"
-artist = "Tiziano Ferro"
 
-#track and artist interpolation
-template = Template("https://lyrist.vercel.app/api/:$track/:$artist")
-url = template.substitute(track = track, artist = artist)
+def getSong(title,author):
+    #track and artist interpolation
+    template = Template("https://lyrist.vercel.app/api/:$track/:$artist")
+    url = template.substitute(track = title, artist = author)
 
-response = requests.get(url)
-data = response.json()
-
-print(data["lyrics"])
+    response = requests.get(url)
+    data = response.json()
+    return data
--- code/text_to_csv.py
+++ code/text_to_csv.py
@@ -1,24 +1,21 @@
 import pandas as pd
-from lyrics_from_title_genius import getSong
+from lyrics_from_title_lyrist import getSong
 import sys
 import re
 
 
 def parseText(Lyrics):
     regex1 = re.compile("\[(.*?)\]", flags=re.VERBOSE) #Elimina tutte le parti tra parentesi
-    regex2 = re.compile("\d+\s*.*Lyrics?",flags= re.VERBOSE) #Elimina la prima parte non utileche restituisce genius API
-    regex3 = re.compile("\d+\s*.*Embed?",flags=re.VERBOSE) #Elimina l'ultima riga non utile che restituisce genius API
-    parsedLyrics = regex1.sub("",Lyrics)
-    parsedLyrics = regex2.sub("",parsedLyrics);
-    parsedLyrics = regex3.sub("",parsedLyrics);
+    parsedLyrics = regex1.sub("",Lyrics);
     return parsedLyrics.split("\n\n")
 
 dict = {'corpus_id':[],'document_id':[],'sentence_id':[],'content':[]}
 song = getSong(sys.argv[1],sys.argv[2])
 
-for indice,strofa in enumerate(parseText(song.lyrics)):
+
+for indice,strofa in enumerate(parseText(song["lyrics"])):
     if(strofa.replace("\n"," ").replace(",","")): 
-        dict['corpus_id'].append(song.title.replace(" ","_"))
+        dict['corpus_id'].append(song["title"].replace(" ","_"))
         dict['document_id'].append(1)
         dict['sentence_id'].append(indice)
         dict['content'].append(strofa.replace("\n"," ").replace(",",""))

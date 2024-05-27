0c395b703364175d470d4d5698ef0bddb8db6aa4
Alessandro <alex.tomaiuolo22@gmail.com>
2024-05-27 19:03:21 +0200

    Fixed index bug in CSV

--- code/text_to_csv.py
+++ code/text_to_csv.py
@@ -14,13 +14,15 @@ def createCSV(author,song):
     dict = {'corpus_id':[],'document_id':[],'sentence_id':[],'content':[]}
     songObject = getSong(author,song)
     arrayStrofe = []
-    for indice,strofa in enumerate(parseText(songObject["lyrics"])):
+    indice = 0
+    for i,strofa in enumerate(parseText(songObject["lyrics"])):
         if(strofa.replace("\n"," ").replace(",","")): 
             dict['corpus_id'].append(songObject["title"].replace(" ","_"))
             dict['document_id'].append(1)
             dict['sentence_id'].append(indice)
             dict['content'].append(strofa.replace("\n"," ").replace(",",""))
             arrayStrofe.append(strofa.replace("\n"," ").replace(",",""))
+            indice = indice + 1 
     df = pd.DataFrame(dict)
     df.to_csv(outputFile, sep=";",index=False) 
     return outputFile, arrayStrofe, songObject["artist"], songObject["title"].replace(" ","_")
